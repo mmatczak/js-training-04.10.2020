abb00c9b802e72b992b3f59acb320ec9affc3242
Marek Matczak <mmatczak@autograf.pl>
2020-11-04 12:42:35 +0100

    methods

--- index.js
+++ index.js
@@ -1,21 +1,12 @@
-// function add() {
-//     let sum = 0;
-//     for (let i = 0; i < arguments.length; i++) {
-//         sum += arguments[i];
-//     }
-//     return sum;
-// }
+const person = {
+    firstName: 'Marek',
+    address: {
+        street: 'Legnicka',
+        city: 'Wroclaw'
+    },
+    sayHello() {
+        return 'Hello, I am ' + this.firstName;
+    }
+}
 
-// const add = function (a, b) {
-//     let sum = 0;
-//     for (let i = 0; i < arguments.length; i++) {
-//         sum += arguments[i];
-//     }
-//     return sum;
-// };
-
-const add = (a, b) => a + b;
-
-const sum = add(2, 3);
-
-console.log(sum);
+console.log(person.sayHello());
