9d1d5af057733bd7b356269a01dc03417b032b28
Marek Matczak <mmatczak@autograf.pl>
2020-11-04 11:19:45 +0100

    object literal

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log('Hello World!');
+const person = {
+    'first-name': 'Marek'
+};
+
+// person.first-name = 'Czarek'
+person['first-name'] = 'Czarek';
+person.lastName = 'Matczak'
+console.log(person);
+delete person.lastName;
+console.log(person);
