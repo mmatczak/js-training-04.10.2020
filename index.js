245fb13a759add6026e8a806be24a24553432ddc
Marek Matczak <mmatczak@autograf.pl>
2020-11-04 12:54:22 +0100

    classes

--- index.js
+++ index.js
@@ -9,4 +9,17 @@ const person = {
     }
 }
 
-console.log(person.sayHello());
+
+class Person {
+    constructor(firstName, lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    sayHello() {
+        return 'Hello, I am ' + this.firstName;
+    }
+}
+
+const marek = new Person('Marek', 'Matczak');
+console.log(marek.sayHello());
